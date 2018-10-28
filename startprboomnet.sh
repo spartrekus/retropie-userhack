0d4fad119b2357d868295d12c73f81d85259d45e
spartrekus <spartrekus@gmail.com>
2018-10-28 12:01:34 +0100

    Update startprboomnet.sh

--- startprboomnet.sh
+++ startprboomnet.sh
@@ -1,5 +1,8 @@
 
 # prboom-plus -iwad /home/pi/doom1.wad  -net 192.168.52.28 
+echo place this file into /home/pi/RetroPie/roms/pc/blabla.sh
+echo runit from msdos icon from the menu
+
 prboom-plus  -net 192.168.52.28 
 
 exit
