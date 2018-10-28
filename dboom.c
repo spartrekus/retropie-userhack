446d2a8c9bc5a964336014c1b0bb074f2ec26998
spartrekus <spartrekus@gmail.com>
2018-10-28 12:00:34 +0100

    Add files via upload

---
+++ dboom.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+int main( int argc, char *argv[])
+{
+    int i ; 
+    system( " prboom-plus -iwad ~/doom1.wad -net 192.168.52.28  " );
+    return 0;
+}
+
+
---
+++ startprboomnet.sh
@@ -0,0 +1,15 @@
+
+# prboom-plus -iwad /home/pi/doom1.wad  -net 192.168.52.28 
+prboom-plus  -net 192.168.52.28 
+
+exit
+
+  prboom-plus -iwad /home/pi/doom1.wad -net 192.168.52.28 
+
+# /opt/retropie/emulators/dosbox/bin/dosbox -startmapper
+##  ///prboom-plus -net 192.168.52.28 
+###  /opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/lr-prboom/prboom_libretro.so --config /opt/retropie/configs/ports/doom/retroarch.cfg /home/pi/RetroPie/roms/ports/doom/prboom.wad --appendconfig /dev/shm/retroarch.cfg
+
+exit
+
+
